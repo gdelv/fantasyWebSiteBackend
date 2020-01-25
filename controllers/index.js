e733e4858e76562f84370d563db783c73dd18d1b
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-01-25 13:46:58 -0500

    added controller to get all users at /api/users

--- controllers/index.js
+++ controllers/index.js
@@ -14,6 +14,16 @@ const createUser = async (req, res) => {
     }
 }
 
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.findAll();
+        return res.status(200).json({ users })
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
 module.exports = {
     createUser,
+    getAllUsers
 }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const controllers = require('../controllers');
 const router = Router();
 
 router.get('/', (req,res) => res.send('This is root!'))
+router.get('/users', controllers.getAllUsers)
 
 router.post('/users', controllers.createUser)
 
