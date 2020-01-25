703f6cf1a10ae3e9405851a20c40d94c1a17c1dc
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-01-25 14:21:12 -0500

    add and update work correctly for users but password shows as null

--- controllers/index.js
+++ controllers/index.js
@@ -38,8 +38,41 @@ const getUserById = async(req, res) => {
     }
 }
 
+const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const [updated] = await User.update(req.body, {
+            where: { id: id }
+        });
+        if(updated) {
+            const updatedUser = await User.findOne({ where: { id: id } });
+            return res.status(200).json({ user: updatedUser });
+        }
+        throw new Error('User not found')
+    } catch(error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleted = await User.destroy({
+            where: { id: id }
+        })
+        if(deleted) {
+            return res.status(204).send('User deleted')
+        }
+        throw new Error('User not found')
+    } catch(error) {
+        return res.status(500).send(error.message)
+    }
+}
+
 module.exports = {
     createUser,
     getAllUsers,
-    getUserById
+    getUserById,
+    updateUser,
+    deleteUser
 }
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const logger = require('morgan')
 const routes = require('./routes')
 
 
 const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(logger('dev'))
 
 app.use('/api', routes)
 
